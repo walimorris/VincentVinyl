579536ee14bb15584ea61ebbf6ec044dc8140889
walimorris <48896622+walimorris@users.noreply.github.com>
2020-02-05 21:05:21 -0800

    Add files via upload

---
+++ Populate_VincentVinyl/QueryAlbum_hasArtist.py
@@ -0,0 +1,41 @@
+import mysql.connector 
+from mysql.connector import Error
+
+try: 
+
+
+    # ESTABLISH A CONNECTION 
+    connection = mysql.connector.connect(
+            host='localhost', 
+            database='VincentVinyl', 
+            user='root',
+            password='Kentstate8$$') 
+
+    # CREATE CURSOR
+    cursor = connection.cursor()
+
+    # CREATE QUERY 
+    albumHasArtist_insert_query = """INSERT INTO Album_has_Artist(Album_idAlbum, Artist_idArtist)
+                             VALUES(%s, %s) """
+
+    # Values for this table include Album id to Artist id
+    albumid_artistid = [(1, 1), 
+                        (2, 2), 
+                        (3, 3), 
+                        (4, 4), 
+                        (5, 5), 
+                        (6, 6)]
+
+    # EXECUTE QUERY
+    cursor.executemany(albumHasArtist_insert_query, albumid_artistid)
+    connection.commit()
+    print(cursor.rowcount,"album Id and artist Id successfully updated into Album_has_artist table")
+
+# CLEAN UP
+except mysql.connector.Error as error: 
+    print("Failed to load data for ablumId and artistId{}".format(error))
+    if(connection.is_connected()): 
+        cursor.close()
+        connection.close()
+
+
